1fe45a77430f34414be009a9920ffff02c43091e
Clarence Adrias <clarence.adrias@gmail.com>
2025-02-21 13:04:02 +0800

    ui

--- src/main/Login.java
+++ src/main/Login.java
@@ -4,6 +4,13 @@
  */
 package main;
 
+
+import admin.Books;
+import dbConnection.DatabaseConnection;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import user.Library;
+
 /**
  *
  * @author Clarence
@@ -26,22 +33,202 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel6 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        loginLBL = new javax.swing.JLabel();
+        usernameLBL = new javax.swing.JLabel();
+        passwordLBL = new javax.swing.JLabel();
+        usernameTXT = new javax.swing.JTextField();
+        loginBTN = new javax.swing.JButton();
+        passwordTXT = new javax.swing.JPasswordField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        stayeaseLBL3 = new javax.swing.JLabel();
+        stayeaseLBL2 = new javax.swing.JLabel();
+        stayeaseLBL = new javax.swing.JLabel();
+        stayeaseLBL4 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jLabel6.setText("jLabel6");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(0, 109, 119));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setBackground(new java.awt.Color(235, 235, 235));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        loginLBL.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        loginLBL.setForeground(new java.awt.Color(10, 61, 98));
+        loginLBL.setText("LOGIN");
+        jPanel2.add(loginLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 140, -1, -1));
+
+        usernameLBL.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        usernameLBL.setForeground(new java.awt.Color(10, 61, 98));
+        usernameLBL.setText("USERNAME");
+        jPanel2.add(usernameLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 220, -1, -1));
+
+        passwordLBL.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        passwordLBL.setForeground(new java.awt.Color(10, 61, 98));
+        passwordLBL.setText("PASSWORD");
+        jPanel2.add(passwordLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, -1, -1));
+        jPanel2.add(usernameTXT, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 250, 270, 30));
+
+        loginBTN.setBackground(new java.awt.Color(0, 109, 119));
+        loginBTN.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        loginBTN.setForeground(new java.awt.Color(245, 245, 220));
+        loginBTN.setText("LOGIN");
+        loginBTN.setToolTipText("");
+        loginBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginBTNActionPerformed(evt);
+            }
+        });
+        jPanel2.add(loginBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 410, 150, 40));
+        jPanel2.add(passwordTXT, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 320, 270, 30));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/login-avatar.png"))); // NOI18N
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 130, -1, -1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/email.png"))); // NOI18N
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 250, -1, 30));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/key.png"))); // NOI18N
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 320, -1, -1));
+
+        jCheckBox1.setBackground(new java.awt.Color(235, 235, 235));
+        jCheckBox1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jCheckBox1.setForeground(new java.awt.Color(10, 61, 98));
+        jCheckBox1.setText("Show Password");
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 360, -1, -1));
+
+        jLabel2.setForeground(new java.awt.Color(10, 61, 98));
+        jLabel2.setText("SIGN IN");
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+        });
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 480, -1, 20));
+
+        jLabel5.setText("Don't have a account?");
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 480, -1, 20));
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 570));
+
+        stayeaseLBL3.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL3.setFont(new java.awt.Font("Serif", 3, 70)); // NOI18N
+        stayeaseLBL3.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL3.setText("S");
+        jPanel1.add(stayeaseLBL3, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 190, 40, -1));
+
+        stayeaseLBL2.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL2.setFont(new java.awt.Font("Serif", 1, 36)); // NOI18N
+        stayeaseLBL2.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL2.setText("TAY ");
+        jPanel1.add(stayeaseLBL2, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 230, 90, 40));
+
+        stayeaseLBL.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL.setFont(new java.awt.Font("Serif", 3, 70)); // NOI18N
+        stayeaseLBL.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL.setText("E");
+        jPanel1.add(stayeaseLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 230, 60, -1));
+
+        stayeaseLBL4.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL4.setFont(new java.awt.Font("Serif", 1, 36)); // NOI18N
+        stayeaseLBL4.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL4.setText("ASE ");
+        jPanel1.add(stayeaseLBL4, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 260, 130, 60));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 570));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+         // TODO add your handling code here:
+        // Action for checkbox, for example, show/hide password when checkbox is checked
+    if (jCheckBox1.isSelected()) {
+        // Show the password by changing the echoChar
+        passwordTXT.setEchoChar((char) 0);  // Show the password in plain text
+    } else {
+        // Hide the password by setting the echoChar
+        passwordTXT.setEchoChar('*');  // Hide the password with asterisks
+    }
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
+    private void loginBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBTNActionPerformed
+        // TODO add your handling code here:
+        String username = usernameTXT.getText();
+    String password = new String(passwordTXT.getPassword()); 
+
+    if (username.isEmpty() || password.isEmpty()) {
+        JOptionPane.showMessageDialog(this, "Please fill in all fields!", "Error", JOptionPane.ERROR_MESSAGE);
+        return;
+    }
+
+    try {
+        // Get the database connection from the DatabaseConnection class
+        Connection conn = DatabaseConnection.getInstance().getConnection();
+
+        // SQL Query
+        String query = "SELECT 'admin' AS role FROM Admin WHERE username = ? AND password = ? " +
+                       "UNION " +
+                       "SELECT 'customer' AS role FROM Customer WHERE username = ? AND password = ?";
+        
+        PreparedStatement pst = conn.prepareStatement(query);
+        pst.setString(1, username);
+        pst.setString(2, password);
+        pst.setString(3, username);
+        pst.setString(4, password);
+        
+        // Execute Query
+        ResultSet rs = pst.executeQuery();
+
+        // Process Result
+        if (rs.next()) {
+            String role = rs.getString("role");
+
+            if (role.equals("admin")) {
+               // JOptionPane.showMessageDialog(this, "Welcome Admin!");
+                new Books().setVisible(true); // Open Admin Dashboard
+            } else {
+               // JOptionPane.showMessageDialog(this, "Welcome Customer!");
+                new Library().setVisible(true); // Open User Dashboard
+            }
+
+            this.dispose(); // Close Login Window
+        } else {
+            JOptionPane.showMessageDialog(this, "Invalid username or password!", "Login Failed", JOptionPane.ERROR_MESSAGE);
+        }
+
+        // Close Connection
+        rs.close();
+        pst.close();
+
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(this, "Database error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    }//GEN-LAST:event_loginBTNActionPerformed
+
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+        Signup signUpFrame = new Signup(); // Assuming SignUp is your frame class
+        signUpFrame.setVisible(true); // Show the sign-up frame
+        this.dispose();
+    }//GEN-LAST:event_jLabel2MouseClicked
 
     /**
      * @param args the command line arguments
@@ -69,6 +256,7 @@ public class Login extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -79,5 +267,24 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton loginBTN;
+    private javax.swing.JLabel loginLBL;
+    private javax.swing.JLabel passwordLBL;
+    private javax.swing.JPasswordField passwordTXT;
+    private javax.swing.JLabel stayeaseLBL;
+    private javax.swing.JLabel stayeaseLBL2;
+    private javax.swing.JLabel stayeaseLBL3;
+    private javax.swing.JLabel stayeaseLBL4;
+    private javax.swing.JLabel usernameLBL;
+    private javax.swing.JTextField usernameTXT;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/main/Signup.java
@@ -0,0 +1,310 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package main;
+
+import dbConnection.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
+public class Signup extends javax.swing.JFrame {
+
+    public Signup() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        stayeaseLBL3 = new javax.swing.JLabel();
+        stayeaseLBL2 = new javax.swing.JLabel();
+        stayeaseLBL = new javax.swing.JLabel();
+        stayeaseLBL4 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        backBTN = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        singupLBL = new javax.swing.JLabel();
+        passwordLBL = new javax.swing.JLabel();
+        fnameLBL = new javax.swing.JLabel();
+        emailLBL = new javax.swing.JLabel();
+        phonenumberLBL = new javax.swing.JLabel();
+        emailTXT = new javax.swing.JTextField();
+        fnameTXT = new javax.swing.JTextField();
+        phonenumberTXT = new javax.swing.JTextField();
+        passwordTXT = new javax.swing.JPasswordField();
+        singupBTN = new javax.swing.JButton();
+        usernameLBL = new javax.swing.JLabel();
+        usernameTXT = new javax.swing.JTextField();
+        stayeaseLBL6 = new javax.swing.JLabel();
+        stayeaseLBL7 = new javax.swing.JLabel();
+        stayeaseLBL1 = new javax.swing.JLabel();
+        stayeaseLBL5 = new javax.swing.JLabel();
+
+        stayeaseLBL3.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL3.setFont(new java.awt.Font("Serif", 3, 70)); // NOI18N
+        stayeaseLBL3.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL3.setText("S");
+
+        stayeaseLBL2.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL2.setFont(new java.awt.Font("Serif", 1, 36)); // NOI18N
+        stayeaseLBL2.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL2.setText("TAY ");
+
+        stayeaseLBL.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL.setFont(new java.awt.Font("Serif", 3, 70)); // NOI18N
+        stayeaseLBL.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL.setText("E");
+
+        stayeaseLBL4.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL4.setFont(new java.awt.Font("Serif", 1, 36)); // NOI18N
+        stayeaseLBL4.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL4.setText("ASE ");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(750, 600));
+
+        jPanel1.setBackground(new java.awt.Color(0, 109, 119));
+        jPanel1.setMinimumSize(new java.awt.Dimension(700, 600));
+        jPanel1.setPreferredSize(new java.awt.Dimension(700, 600));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        backBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/back.png"))); // NOI18N
+        backBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backBTNActionPerformed(evt);
+            }
+        });
+        jPanel1.add(backBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 60, 40));
+
+        jPanel2.setMinimumSize(new java.awt.Dimension(310, 600));
+        jPanel2.setPreferredSize(new java.awt.Dimension(310, 600));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        singupLBL.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        singupLBL.setText("SIGN UP");
+        jPanel2.add(singupLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 30, 100, -1));
+
+        passwordLBL.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        passwordLBL.setText("PASSWORD:");
+        jPanel2.add(passwordLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 370, -1, -1));
+
+        fnameLBL.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        fnameLBL.setText("FULL NAME:");
+        jPanel2.add(fnameLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 80, -1, -1));
+
+        emailLBL.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        emailLBL.setText("EMAIL:");
+        jPanel2.add(emailLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 230, -1, -1));
+
+        phonenumberLBL.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        phonenumberLBL.setText("PHONE NUMBER:");
+        jPanel2.add(phonenumberLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 160, -1, -1));
+        jPanel2.add(emailTXT, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 260, 240, 30));
+
+        fnameTXT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fnameTXTActionPerformed(evt);
+            }
+        });
+        jPanel2.add(fnameTXT, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 110, 240, 30));
+        jPanel2.add(phonenumberTXT, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 190, 240, 30));
+
+        passwordTXT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passwordTXTActionPerformed(evt);
+            }
+        });
+        jPanel2.add(passwordTXT, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 400, 240, 30));
+
+        singupBTN.setBackground(new java.awt.Color(0, 109, 119));
+        singupBTN.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        singupBTN.setForeground(new java.awt.Color(245, 245, 220));
+        singupBTN.setText("SIGN UP");
+        singupBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                singupBTNActionPerformed(evt);
+            }
+        });
+        jPanel2.add(singupBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 470, -1, -1));
+
+        usernameLBL.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        usernameLBL.setText("USERNAME:");
+        jPanel2.add(usernameLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 300, -1, -1));
+
+        usernameTXT.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                usernameTXTActionPerformed(evt);
+            }
+        });
+        jPanel2.add(usernameTXT, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, 240, 30));
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(375, 0, 375, 600));
+
+        stayeaseLBL6.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL6.setFont(new java.awt.Font("Serif", 1, 36)); // NOI18N
+        stayeaseLBL6.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL6.setText("TAY ");
+        jPanel1.add(stayeaseLBL6, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 250, 90, 40));
+
+        stayeaseLBL7.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL7.setFont(new java.awt.Font("Serif", 1, 36)); // NOI18N
+        stayeaseLBL7.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL7.setText("ASE ");
+        jPanel1.add(stayeaseLBL7, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 280, 130, 60));
+
+        stayeaseLBL1.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL1.setFont(new java.awt.Font("Serif", 3, 70)); // NOI18N
+        stayeaseLBL1.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL1.setText("E");
+        jPanel1.add(stayeaseLBL1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 250, 60, -1));
+
+        stayeaseLBL5.setBackground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL5.setFont(new java.awt.Font("Serif", 3, 70)); // NOI18N
+        stayeaseLBL5.setForeground(new java.awt.Color(245, 245, 220));
+        stayeaseLBL5.setText("S");
+        jPanel1.add(stayeaseLBL5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 220, 40, -1));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void backBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backBTNActionPerformed
+        Login loginFrame = new Login();
+        this.setVisible(false);
+        loginFrame.setVisible(true);
+    }//GEN-LAST:event_backBTNActionPerformed
+
+    private void passwordTXTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordTXTActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_passwordTXTActionPerformed
+
+    private void singupBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_singupBTNActionPerformed
+        // TODO add your handling code here:
+        // Get values from text fields
+    String fullName = fnameTXT.getText();
+    String phoneNumber = phonenumberTXT.getText();
+    String email = emailTXT.getText();
+    String username = usernameTXT.getText();
+    String password = new String(passwordTXT.getPassword()); // Convert char array to String
+
+    // Database connection
+    Connection conn = DatabaseConnection.getInstance().getConnection();
+    
+    if (conn != null) {
+        try {
+            // Insert query
+            String sql = "INSERT INTO customer (fullName, phoneNumber, email, username, password) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            
+            // Set values
+            pstmt.setString(1, fullName);
+            pstmt.setString(2, phoneNumber);
+            pstmt.setString(3, email);
+            pstmt.setString(4, username);
+            pstmt.setString(5, password);
+            
+            // Execute the query
+            int rowsInserted = pstmt.executeUpdate();
+            if (rowsInserted > 0) {
+                
+                JOptionPane.showMessageDialog(this, "Signup Successful!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                new Login().setVisible(true);
+                this.dispose();
+            } else {
+                JOptionPane.showMessageDialog(this, "Signup Failed!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            
+            pstmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Database Error: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    } else {
+        JOptionPane.showMessageDialog(this, "Database connection failed!", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    }//GEN-LAST:event_singupBTNActionPerformed
+
+    private void fnameTXTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fnameTXTActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_fnameTXTActionPerformed
+
+    private void usernameTXTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usernameTXTActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_usernameTXTActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Signup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Signup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Signup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Signup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Signup().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton backBTN;
+    private javax.swing.JLabel emailLBL;
+    private javax.swing.JTextField emailTXT;
+    private javax.swing.JLabel fnameLBL;
+    private javax.swing.JTextField fnameTXT;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel passwordLBL;
+    private javax.swing.JPasswordField passwordTXT;
+    private javax.swing.JLabel phonenumberLBL;
+    private javax.swing.JTextField phonenumberTXT;
+    private javax.swing.JButton singupBTN;
+    private javax.swing.JLabel singupLBL;
+    private javax.swing.JLabel stayeaseLBL;
+    private javax.swing.JLabel stayeaseLBL1;
+    private javax.swing.JLabel stayeaseLBL2;
+    private javax.swing.JLabel stayeaseLBL3;
+    private javax.swing.JLabel stayeaseLBL4;
+    private javax.swing.JLabel stayeaseLBL5;
+    private javax.swing.JLabel stayeaseLBL6;
+    private javax.swing.JLabel stayeaseLBL7;
+    private javax.swing.JLabel usernameLBL;
+    private javax.swing.JTextField usernameTXT;
+    // End of variables declaration//GEN-END:variables
+}
